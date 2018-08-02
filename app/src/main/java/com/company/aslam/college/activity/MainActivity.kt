a43a2be0c3fd4163865fe32333a54f7c05724917
Aslam Rasheed <aslampr07@outlook.com>
2018-08-02 20:25:01 +0530

    Added banner

---
+++ app/src/main/java/com/company/aslam/college/activity/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.company.aslam.college.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
+import com.company.aslam.college.R
+import com.company.aslam.college.adapter.HomeBannerViewPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainToolbar = findViewById<Toolbar>(R.id.Home_ActionBar)
+        val bannerPager = findViewById<ViewPager>(R.id.Home_MainBanner)
+        //setting the actionbar
+        setSupportActionBar(mainToolbar)
+
+        bannerPager.adapter = HomeBannerViewPagerAdapter(supportFragmentManager)
+    }
+
+}
---
+++ app/src/main/java/com/company/aslam/college/adapter/HomeBannerViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.company.aslam.college.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import com.company.aslam.college.BannerFragment
+
+class HomeBannerViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm){
+
+    init {
+        //This is a constructor
+    }
+
+    override fun getItem(p0: Int): Fragment {
+        return BannerFragment()
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+}
