cab6351d80144062cd86fca1538fc2e0643cba95
Aslam Rasheed <aslampr07@outlook.com>
2018-08-07 14:42:26 +0530

    Added dotted tablayout on banner.

--- app/src/main/java/com/company/aslam/college/activity/MainActivity.kt
+++ app/src/main/java/com/company/aslam/college/activity/MainActivity.kt
@@ -2,19 +2,18 @@ package com.company.aslam.college.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.widget.TableLayout
 import com.company.aslam.college.R
 import com.company.aslam.college.adapter.BulletinBoardAdapter
 import com.company.aslam.college.adapter.HomeBannerViewPagerAdapter
 import com.company.aslam.college.data.BulletinBoard
 
 class MainActivity : AppCompatActivity() {
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +22,19 @@ class MainActivity : AppCompatActivity() {
         val mainToolbar = findViewById<Toolbar>(R.id.Home_ActionBar)
         val bannerPager = findViewById<ViewPager>(R.id.Home_MainBanner)
         val bulletinRecycler = findViewById<RecyclerView>(R.id.Home_BulletinBoard)
+        val dottedTab = findViewById<TabLayout>(R.id.Home_DottedTab)
 
         //setting the actionbar
         setSupportActionBar(mainToolbar)
 
         bannerPager.adapter = HomeBannerViewPagerAdapter(supportFragmentManager)
+        dottedTab.setupWithViewPager(bannerPager)
 
         val bulletinList : MutableList<BulletinBoard> = arrayListOf()
         for (i in 0..3){
-            bulletinList.add(BulletinBoard("Heading $i", "This is empty empty body"))
+            bulletinList.add(BulletinBoard("Heading ${i+1}", "This is empty empty body"))
         }
         bulletinRecycler.layoutManager = LinearLayoutManager(this)
         bulletinRecycler.adapter = BulletinBoardAdapter(bulletinList)
     }
-
 }
